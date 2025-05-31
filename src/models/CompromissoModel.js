0bfdf5c2dcfccecca2572cda51d11f86116beabb
Paulo Henrique Trentin <148505377+PauloHenriqueTrentin@users.noreply.github.com>
2025-05-30 21:03:03 -0300

    Altera-Texto

--- src/models/CompromissoModel.js
+++ src/models/CompromissoModel.js
@@ -1,4 +1,4 @@
-import { DataTypes } from "sequelize";Add commentMore actions
+import { DataTypes } from "sequelize";
 import { sequelize } from "../config/config.js";
 import Contact from "./ContactModel.js";
 
